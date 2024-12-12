04375ea885aeb95a3616c5a5d826c479030a3a80
Julien Marrec <julien.marrec@gmail.com>
2024-12-12 17:07:11 +0100

    Add a dynamic test to ensure that ALL possible Curves Input/Output Unit Types are accepted: they aren't
    ```
    [ RUN      ] EnergyPlusFixture.AllPossibleUnitTypeValid
    /home/julien/Software/Others/EnergyPlus2/tst/EnergyPlus/unit/CurveManager.unit.cc:967: Failure
    Value of: Curve::IsCurveInputTypeValid(input_unit_type)
      Actual: false
    Expected: true
    VolumetricFlowPerPower is rejected by IsCurveOutputTypeValid
    ```

--- tst/EnergyPlus/unit/CurveManager.unit.cc
+++ tst/EnergyPlus/unit/CurveManager.unit.cc
@@ -745,6 +745,25 @@ TEST_F(EnergyPlusFixture, CSV_CarriageReturns_Handling)
     }
 }
 
+nlohmann::json const &getPatternProperties(nlohmann::json const &schema_obj)
+{
+    auto const &pattern_properties = schema_obj["patternProperties"];
+    int dot_star_present = pattern_properties.count(".*");
+    std::string pattern_property;
+    if (dot_star_present > 0) {
+        pattern_property = ".*";
+    } else {
+        int no_whitespace_present = pattern_properties.count(R"(^.*\S.*$)");
+        if (no_whitespace_present > 0) {
+            pattern_property = R"(^.*\S.*$)";
+        } else {
+            throw std::runtime_error(R"(The patternProperties value is not a valid choice (".*", "^.*\S.*$"))");
+        }
+    }
+    auto const &schema_obj_props = pattern_properties[pattern_property]["properties"];
+    return schema_obj_props;
+}
+
 std::vector<std::string> getPossibleChoicesFromSchema(const std::string &objectType, const std::string &fieldName)
 {
     // Should consider making this public, at least to the EnergyPlusFixture, but maybe in the InputProcessor directly
@@ -752,20 +771,7 @@ std::vector<std::string> getPossibleChoicesFromSchema(const std::string &objectT
     static const auto json_schema = nlohmann::json::from_cbor(EmbeddedEpJSONSchema::embeddedEpJSONSchema());
     auto const &schema_properties = json_schema.at("properties");
     const auto &object_schema = schema_properties.at(objectType);
-    auto const &pattern_properties = object_schema["patternProperties"];
-    int dot_star_present = pattern_properties.count(".*");
-    std::string pattern_property;
-    if (dot_star_present) {
-        pattern_property = ".*";
-    } else {
-        int no_whitespace_present = pattern_properties.count(R"(^.*\S.*$)");
-        if (no_whitespace_present) {
-            pattern_property = R"(^.*\S.*$)";
-        } else {
-            throw std::runtime_error(R"(The patternProperties value is not a valid choice (".*", "^.*\S.*$"))");
-        }
-    }
-    auto const &schema_obj_props = pattern_properties[pattern_property]["properties"];
+    auto const &schema_obj_props = getPatternProperties(object_schema);
     auto const &schema_field_obj = schema_obj_props.at(fieldName);
     std::vector<std::string> choices;
     for (const auto &e : schema_field_obj.at("enum")) {
@@ -917,3 +923,54 @@ INSTANTIATE_TEST_SUITE_P(CurveManager,
                              }
                              return std::string{info.param};
                          });
+
+std::pair<std::set<std::string>, std::set<std::string>> getAllPossibleInputOutputTypesForCurves()
+{
+    const auto json_schema = nlohmann::json::from_cbor(EmbeddedEpJSONSchema::embeddedEpJSONSchema());
+    auto const &schema_properties = json_schema.at("properties");
+    std::set<std::string> all_input_choices;
+    std::set<std::string> all_output_choices;
+
+    for (const auto &[objectType, object_schema] : schema_properties.items()) {
+        const bool is_curve = (objectType.rfind("Curve:", 0) == 0) || (objectType == "Table:Lookup") || (objectType == "Table:IndependentVariable");
+        if (!is_curve) {
+            continue;
+        }
+        auto const &schema_obj_props = getPatternProperties(object_schema);
+        for (const auto &[fieldName, schema_field_obj] : schema_obj_props.items()) {
+            if (std::string(fieldName) == "output_unit_type") {
+                for (const auto &e : schema_field_obj.at("enum")) {
+                    all_output_choices.insert(std::string{e});
+                }
+            } else if (fieldName.find("unit_type") != std::string::npos) {
+                for (const auto &e : schema_field_obj.at("enum")) {
+                    all_input_choices.insert(std::string{e});
+                }
+            }
+        }
+    }
+
+    return {all_input_choices, all_output_choices};
+}
+
+TEST_F(EnergyPlusFixture, AllPossibleUnitTypeValid)
+{
+    auto const [all_input_choices, all_output_choices] = getAllPossibleInputOutputTypesForCurves();
+
+    // As of 2024-12-18
+    // in = ["", "Angle", "Dimensionless", "Distance", "MassFlow", "Power", "Pressure", "Temperature", "VolumetricFlow","VolumetricFlowPerPower"]
+    // out = ["", "Capacity", "Dimensionless", "Power", "Pressure", "Temperature"]
+    EXPECT_FALSE(all_input_choices.empty()) << fmt::format("{}", all_input_choices);
+    EXPECT_FALSE(all_output_choices.empty()) << fmt::format("{}", all_output_choices);
+
+    for (const auto &input_unit_type : all_input_choices) {
+        EXPECT_TRUE(Curve::IsCurveInputTypeValid(input_unit_type)) << input_unit_type << " is rejected by IsCurveOutputTypeValid";
+    }
+
+    for (const auto &output_unit_type : all_output_choices) {
+        if (output_unit_type.empty()) {
+            continue;
+        }
+        EXPECT_TRUE(Curve::IsCurveOutputTypeValid(output_unit_type)) << output_unit_type << " is rejected by IsCurveOutputTypeValid";
+    }
+}
